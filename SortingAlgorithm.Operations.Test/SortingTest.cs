7e82e55e684cbe39e7b9fd2ee5ceef5bdc13ecc7
Ali Raza <aliraza1567@gmail.com>
2017-10-26 01:12:37 +0200

    Added Test Project

---
+++ SortingAlgorithm.Operations.Test/SortingTest.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OhpenTest.Operations.Convert;
+using OhpenTest.Operations.Sorting;
+
+namespace OhpenTest.Operations.Test
+{
+    [TestClass]
+    public class SortingTest
+    {
+        public static string TestString { get; } = "I w-a-n-t t-o work at some company A-S-A-P";
+        public static string ExpectedResult { get; } = "A-S-A-P at I some company t-o w-a-n-t work";
+        private ISorting _sorting;
+
+        [TestMethod]
+        public void SelectionSortTest()
+        {
+            _sorting = new Sorting.Sorting();
+
+            var actual = Conversion.ArrayToString(_sorting.SelectionSort(Conversion.StringToArray(TestString)));
+
+            Assert.AreEqual(ExpectedResult, actual);
+        }
+
+        [TestMethod]
+        public void InsertionSortTest()
+        {
+            _sorting = new Sorting.Sorting();
+
+            var actual = Conversion.ArrayToString(_sorting.InsertionSort(Conversion.StringToArray(TestString)));
+
+            Assert.AreEqual(ExpectedResult, actual);
+        }
+
+        [TestMethod]
+        public void BubbleSortTest()
+        {
+            _sorting = new Sorting.Sorting();
+
+            var actual = Conversion.ArrayToString(_sorting.BubbleSort(Conversion.StringToArray(TestString)));
+
+            Assert.AreEqual(ExpectedResult, actual);
+        }
+    }
+}
